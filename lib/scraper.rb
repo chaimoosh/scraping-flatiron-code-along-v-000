6236d21e9f871d85f3796f57054c661be0eae9ae
Chaim Reisman <crreisman@gmail.com>
2018-02-13 15:18:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,7 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
+    doc.css(".post").first.css("h2").text
   end 
   
   def print_courses
